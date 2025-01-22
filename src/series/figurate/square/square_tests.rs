83932a628b26bca2f65b2315fc2865bc0534f1fa
indrajit <eendroroy@gmail.com>
2025-01-22 20:03:24 +0600

    square: add unit tests

---
+++ src/series/figurate/square/square_tests.rs
@@ -0,0 +1,50 @@
+#[test]
+fn should_generate_nth_square() {
+    use super::*;
+    assert_eq!(nth_squire(1), 1u128.into());
+    assert_eq!(nth_squire(2), 4u128.into());
+    assert_eq!(nth_squire(3), 9u128.into());
+    assert_eq!(nth_squire(4), 16u128.into());
+    assert_eq!(nth_squire(5), 25u128.into());
+    assert_eq!(nth_squire(6), 36u128.into());
+    assert_eq!(nth_squire(7), 49u128.into());
+    assert_eq!(nth_squire(8), 64u128.into());
+    assert_eq!(nth_squire(9), 81u128.into());
+    assert_eq!(nth_squire(10), 100u128.into());
+}
+
+#[test]
+fn should_generate_series_of_1_items() {
+    use super::*;
+    assert_eq!(square_sequence(1), vec![1u128.into()]);
+}
+
+#[test]
+fn should_generate_series_of_20_items() {
+    use super::*;
+    assert_eq!(
+        square_sequence(20),
+        vec![
+            1u128.into(),
+            4u128.into(),
+            9u128.into(),
+            16u128.into(),
+            25u128.into(),
+            36u128.into(),
+            49u128.into(),
+            64u128.into(),
+            81u128.into(),
+            100u128.into(),
+            121u128.into(),
+            144u128.into(),
+            169u128.into(),
+            196u128.into(),
+            225u128.into(),
+            256u128.into(),
+            289u128.into(),
+            324u128.into(),
+            361u128.into(),
+            400u128.into()
+        ]
+    );
+}
