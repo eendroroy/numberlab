0b9dc9bcd0a24616a14ab73a60eed661bb6b18b1
indrajit <eendroroy@gmail.com>
2025-01-22 20:06:06 +0600

    fibonacci: update bench

--- benches/fibonacci.rs
+++ benches/fibonacci.rs
@@ -8,16 +8,23 @@ fn generate_fibonacci_series_of_10(c: &mut Criterion) {
     });
 }
 
-fn generate_fibonacci_series_of_5000(c: &mut Criterion) {
-    c.bench_function("generate_fibonacci_series_of_5000", |b| {
-        b.iter(|| fibonacci_sequence(BigUint::from(0u128), 10));
+fn generate_fibonacci_series_of_500(c: &mut Criterion) {
+    c.bench_function("generate_fibonacci_series_of_500", |b| {
+        b.iter(|| fibonacci_sequence(BigUint::from(0u128), 500));
+    });
+}
+
+fn generate_fibonacci_series_of_1000(c: &mut Criterion) {
+    c.bench_function("generate_fibonacci_series_of_1000", |b| {
+        b.iter(|| fibonacci_sequence(BigUint::from(0u128), 1000));
     });
 }
 
 criterion_group!(
     benches,
     generate_fibonacci_series_of_10,
-    generate_fibonacci_series_of_5000
+    generate_fibonacci_series_of_500,
+    generate_fibonacci_series_of_1000,
 );
 
 criterion_main!(benches);
