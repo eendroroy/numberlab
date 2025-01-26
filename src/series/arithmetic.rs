092fceb90cfac25022292efc84cefa18155180e7
indrajit <eendroroy@gmail.com>
2025-01-26 13:10:16 +0600

    arithmetic: add arithmetic_sum and nth_arithmetic functions

--- src/series/arithmetic.rs
+++ src/series/arithmetic.rs
@@ -1,24 +1,39 @@
 use std::cmp::max;
 use std::ops::{Add, Div, Mul};
 
-pub fn arithmetic_sequence(starting_number: f64, progression: f64, n: u128) -> Vec<f64> {
-    let precision = max(
+fn precision(starting_number: f64, progression: f64) -> u32 {
+    max(
         starting_number.to_string().split(".").last().unwrap().len(),
         progression.to_string().split(".").last().unwrap().len(),
-    ) as u32;
+    ) as u32
+}
+
+pub fn nth_arithmetic(starting_number: f64, progression: f64, n: u128) -> f64 {
+    let precision = precision(starting_number, progression);
+    let nth = starting_number
+        .add(progression.mul(n as f64))
+        .mul(10_i32.pow(precision) as f64)
+        .round()
+        .div(10_i32.pow(precision) as f64);
+    nth
+}
+
+pub fn arithmetic_sequence(starting_number: f64, progression: f64, n: u128) -> Vec<f64> {
     let mut sequence = vec![starting_number];
     for _ in 1..n {
-        let ith = sequence
-            .last()
-            .unwrap()
-            .add(progression)
-            .mul(10_i32.pow(precision) as f64)
-            .round()
-            .div(10_i32.pow(precision) as f64);
+        let ith = nth_arithmetic(sequence.last().unwrap().clone(), progression, 1);
 
         sequence.push(ith);
     }
     sequence
+}
+
+pub fn arithmetic_sum(starting_number: f64, progression: f64, n: u64) -> f64 {
+    let precision = precision(starting_number, progression);
+    (((2.0 * starting_number + ((n as f64) - 1.0) * progression) * (n as f64)) / 2.0)
+        .mul(10_i32.pow(precision) as f64)
+        .round()
+        .div(10_i32.pow(precision) as f64)
 }
 
 #[cfg(test)]
--- src/series/arithmetic/arithmetic_test.rs
+++ src/series/arithmetic/arithmetic_test.rs
@@ -1,10 +1,44 @@
 use super::*;
 
 #[test]
-fn test_arithmetic_sequence() {
+fn should_calculate_nth_arithmetic() {
+    assert_eq!(nth_arithmetic(0f64, 1f64, 0), 0f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 1), 1f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 2), 2f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 3), 3f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 4), 4f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 5), 5f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 6), 6f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 7), 7f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 8), 8f64);
+    assert_eq!(nth_arithmetic(0f64, 1f64, 9), 9f64);
+}
+
+#[test]
+fn should_generate_arithmetic_sequence() {
     let sequence = arithmetic_sequence(0f64, 1f64, 10);
-    assert_eq!(sequence, vec![0f64, 1f64, 2f64, 3f64, 4f64, 5f64, 6f64, 7f64, 8f64, 9f64]);
+    assert_eq!(
+        sequence,
+        vec![0f64, 1f64, 2f64, 3f64, 4f64, 5f64, 6f64, 7f64, 8f64, 9f64]
+    );
 
     let sequence = arithmetic_sequence(5.4, -2.92, 10);
-    assert_eq!(sequence, vec![5.4, 2.48, -0.44, -3.36, -6.28, -9.2, -12.12, -15.04, -17.96, -20.88]);
+    assert_eq!(
+        sequence,
+        vec![5.4, 2.48, -0.44, -3.36, -6.28, -9.2, -12.12, -15.04, -17.96, -20.88]
+    );
+}
+
+#[test]
+fn should_calculate_arithmetic_sum() {
+    assert_eq!(arithmetic_sum(0f64, 1f64, 1), 0f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 2), 1f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 3), 3f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 4), 6f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 5), 10f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 6), 15f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 7), 21f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 8), 28f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 9), 36f64);
+    assert_eq!(arithmetic_sum(0f64, 1f64, 10), 45f64);
 }
