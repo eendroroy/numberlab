8df496837ba09f32203c02555cded050199005be
indrajit <eendroroy@gmail.com>
2025-01-27 15:48:52 +0600

    update arithmetic series

--- src/series/arithmetic.rs
+++ src/series/arithmetic.rs
@@ -2,7 +2,7 @@
 ///
 /// # Arguments
 ///
-/// * `a1` - The first term of the sequence.
+/// * `a` - The first term of the sequence.
 /// * `d` - The common difference between terms.
 /// * `n` - The term number to calculate.
 ///
@@ -15,20 +15,20 @@
 /// ```
 /// use numseries::series::arithmetic::nth_arithmetic;
 ///
-/// let a1 = 2.0;
+/// let a = 2.0;
 /// let d = 3.0;
 /// let n = 4;
-/// let nth_term = nth_arithmetic(a1, d, n);
+/// let nth_term = nth_arithmetic(a, d, n);
 /// ```
-pub fn nth_arithmetic(a1: f64, d: f64, n: u128) -> f64 {
-    a1 + (d * n as f64)
+pub fn nth_arithmetic(a: f64, d: f64, n: u128) -> f64 {
+    a + (d * n as f64)
 }
 
 /// Generates an arithmetic sequence.
 ///
 /// # Arguments
 ///
-/// * `a1` - The first term of the sequence.
+/// * `a` - The first term of the sequence.
 /// * `d` - The common difference between terms.
 /// * `n` - The number of terms to generate.
 ///
@@ -41,15 +41,15 @@ pub fn nth_arithmetic(a1: f64, d: f64, n: u128) -> f64 {
 /// ```
 /// use numseries::series::arithmetic::arithmetic_sequence;
 ///
-/// let a1 = 2.0;
+/// let a = 2.0;
 /// let d = 3.0;
 /// let n = 4;
-/// let sequence = arithmetic_sequence(a1, d, n);
+/// let sequence = arithmetic_sequence(a, d, n);
 /// assert_eq!(sequence, vec![2.0, 5.0, 8.0, 11.0]);
 /// ```
-pub fn arithmetic_sequence(a1: f64, d: f64, n: u128) -> Vec<f64> {
-    let mut sequence = vec![a1];
-    (1..n).for_each(|i| sequence.push(nth_arithmetic(a1, d, i)));
+pub fn arithmetic_sequence(a: f64, d: f64, n: u128) -> Vec<f64> {
+    let mut sequence = vec![a];
+    (1..n).for_each(|i| sequence.push(nth_arithmetic(a, d, i)));
     sequence
 }
 
@@ -57,7 +57,7 @@ pub fn arithmetic_sequence(a1: f64, d: f64, n: u128) -> Vec<f64> {
 ///
 /// # Arguments
 ///
-/// * `a1` - The first term of the sequence.
+/// * `a` - The first term of the sequence.
 /// * `d` - The common difference between terms.
 /// * `n` - The number of terms to sum.
 ///
@@ -70,13 +70,13 @@ pub fn arithmetic_sequence(a1: f64, d: f64, n: u128) -> Vec<f64> {
 /// ```
 /// use numseries::series::arithmetic::arithmetic_sum;
 ///
-/// let a1 = 5.4;
+/// let a = 5.4;
 /// let d = -2.9233;
 /// let n = 10;
-/// let sum = arithmetic_sum(a1, d, n);
+/// let sum = arithmetic_sum(a, d, n);
 /// ```
-pub fn arithmetic_sum(a1: f64, d: f64, n: u64) -> f64 {
-    ((2.0 * a1 + ((n as f64) - 1.0) * d) * (n as f64)) / 2.0
+pub fn arithmetic_sum(a: f64, d: f64, n: u64) -> f64 {
+    ((2.0 * a + ((n as f64) - 1.0) * d) * (n as f64)) / 2.0
 }
 
 #[cfg(test)]
