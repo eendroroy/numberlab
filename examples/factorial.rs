6057e022998127600fb5b578d8be4c7d83e96e64
indrajit <eendroroy@gmail.com>
2025-01-27 15:00:56 +0600

    update factorial series

--- examples/factorial.rs
+++ examples/factorial.rs
@@ -1,6 +1,9 @@
-use numseries::series::factorial::factorial_sequence;
+use numseries::series::factorial::{factorial_sequence, nth_factorial_with_memoizer};
+use numseries::series::fibonacci::nth_fibonacci_memoized;
 
 fn main() {
+    println!("11! : {}", nth_factorial_with_memoizer(10));
+
     factorial_sequence(10)
         .iter()
         .for_each(|n| println!("{}", n));
--- src/series/factorial.rs
+++ src/series/factorial.rs
@@ -1,11 +1,22 @@
 use num_bigint::BigUint;
 use std::ops::Mul;
 
-pub fn nth_factorial_memoized(n: usize, memoizer: &mut Vec<BigUint>) -> BigUint {
+pub fn nth_factorial(n: usize) -> BigUint {
+    (1..=n)
+        .map(|i| BigUint::from(i))
+        .fold(BigUint::from(1u128), |acc, x| acc.mul(x))
+}
+
+pub fn nth_factorial_memoized(n: usize) -> BigUint {
+    let mut memoizer = vec![BigUint::from(1u128)];
+    nth_factorial_with_memoizer(n, &mut memoizer)
+}
+
+pub fn nth_factorial_with_memoizer(n: usize, memoizer: &mut Vec<BigUint>) -> BigUint {
     if n < memoizer.len() {
         memoizer[n].clone()
     } else {
-        let mth_factorial = nth_factorial_memoized(n - 1, memoizer);
+        let mth_factorial = nth_factorial_with_memoizer(n - 1, memoizer);
         let nth_factorial = mth_factorial.mul(n);
         memoizer.push(nth_factorial);
         memoizer[n].clone()
@@ -13,11 +24,15 @@ pub fn nth_factorial_memoized(n: usize, memoizer: &mut Vec<BigUint>) -> BigUint 
 }
 
 pub fn factorial_sequence(n: usize) -> Vec<BigUint> {
-    let mut sequence = vec![BigUint::from(1u128)];
-    for i in 0..n {
-        nth_factorial_memoized(i, &mut sequence);
+    if n == 0 {
+        vec![]
+    } else if n == 1 {
+        vec![BigUint::from(1u128)]
+    } else {
+        let mut sequence = vec![BigUint::from(1u128)];
+        (1..n).for_each(|i| sequence.push(&sequence[i - 1] * BigUint::from(i)));
+        sequence[..n].to_vec()
     }
-    sequence[..n].to_vec()
 }
 
 #[cfg(test)]
