c5ba90d4fa954a31ea8defae926c0c0f422fbb53
indrajit <eendroroy@gmail.com>
2025-01-27 15:28:16 +0600

    update factorial example

--- examples/factorial.rs
+++ examples/factorial.rs
@@ -1,8 +1,7 @@
-use numseries::series::factorial::{factorial_sequence, nth_factorial_with_memoizer};
-use numseries::series::fibonacci::nth_fibonacci_memoized;
+use numseries::series::factorial::{factorial_sequence, nth_factorial_memoized};
 
 fn main() {
-    println!("11! : {}", nth_factorial_with_memoizer(10));
+    println!("11! : {}", nth_factorial_memoized(10));
 
     factorial_sequence(10)
         .iter()
