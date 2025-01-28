c0449441245f77fb858a4c56fac879503eb025c1
indrajit <eendroroy@gmail.com>
2025-01-28 18:31:36 +0600

    update pentagonal sequence

--- examples/pentagonal.rs
+++ examples/pentagonal.rs
@@ -1,7 +1,10 @@
-use numberlab::sequence::figurate::pentagonal::pentagonal_sequence;
+use num_bigint::BigUint;
+use numberlab::sequence::figurate::pentagonal::{nth_pentagonal, pentagonal_sequence};
 
 fn main() {
-    pentagonal_sequence(10)
-        .iter()
-        .for_each(|n| println!("{}", n));
+    let sequence = pentagonal_sequence(10);
+    println!("{:?} == {}", sequence, sequence.iter().sum::<BigUint>());
+
+    let n_sequence: Vec<BigUint> = (1..=10).map(|n| nth_pentagonal(n)).collect();
+    println!("{:?} == {}", n_sequence, n_sequence.iter().sum::<BigUint>());
 }
--- src/sequence/figurate/pentagonal.rs
+++ src/sequence/figurate/pentagonal.rs
@@ -1,16 +1,62 @@
+use crate::big_u;
 use num_bigint::BigUint;
 use std::ops::{Div, Mul, Sub};
 
+/// Calculates the nth pentagonal number.
+///
+/// # Arguments
+///
+/// * `n` - A non-negative integer representing the position in the pentagonal sequence.
+///
+/// # Returns
+///
+/// The nth pentagonal number as a `BigUint`.
+///
+/// # Examples
+///
+/// ```
+/// use num_bigint::BigUint;
+/// use numberlab::sequence::figurate::pentagonal::nth_pentagonal;
+///
+/// let result = nth_pentagonal(10);
+/// assert_eq!(result, BigUint::from(145u128));
+/// ```
 pub fn nth_pentagonal(n: u128) -> BigUint {
-    BigUint::from(n)
-        .mul(BigUint::from(n).mul(3u128).sub(BigUint::from(1u128)))
-        .div(BigUint::from(2u128))
+    big_u!(n)
+        .mul(big_u!(n).mul(big_u!(3u128)).sub(big_u!(1u128)))
+        .div(big_u!(2u128))
 }
 
+/// Generates a sequence of pentagonal numbers up to the given number `n`.
+///
+/// # Arguments
+///
+/// * `n` - A non-negative integer representing the number of terms in the pentagonal sequence.
+///
+/// # Returns
+///
+/// A vector containing the pentagonal sequence up to the given number `n`.
+///
+/// # Examples
+///
+/// ```
+/// use num_bigint::BigUint;
+/// use numberlab::sequence::figurate::pentagonal::pentagonal_sequence;
+///
+/// let sequence = pentagonal_sequence(5);
+/// assert_eq!(
+///     sequence,
+///     vec![
+///         BigUint::from(1_u128),
+///         BigUint::from(5_u128),
+///         BigUint::from(12_u128),
+///         BigUint::from(22_u128),
+///         BigUint::from(35_u128)
+///     ]
+/// );
+/// ```
 pub fn pentagonal_sequence(n: u128) -> Vec<BigUint> {
     let mut sequence = vec![];
-    for i in 1..=n {
-        sequence.push(nth_pentagonal(i));
-    }
+    (1..=n).for_each(|i| sequence.push(nth_pentagonal(i)));
     sequence
 }
