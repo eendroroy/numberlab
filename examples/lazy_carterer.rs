159c4fcedea1634a81d875419b2e42ca84d68b04
indrajit <eendroroy@gmail.com>
2025-01-28 18:24:36 +0600

    update lazy_caterer sequence

--- examples/lazy_carterer.rs
+++ examples/lazy_carterer.rs
@@ -1,7 +1,10 @@
-use numberlab::sequence::figurate::lazy_caterer::lazy_caterer_sequence;
+use num_bigint::BigUint;
+use numberlab::sequence::figurate::lazy_caterer::{lazy_caterer_sequence, nth_lazy_caterer};
 
 fn main() {
-    lazy_caterer_sequence(10)
-        .iter()
-        .for_each(|n| println!("{}", n));
+    let sequence = lazy_caterer_sequence(10);
+    println!("{:?} == {}", sequence, sequence.iter().sum::<BigUint>());
+
+    let n_sequence: Vec<BigUint> = (0..10).map(|n| nth_lazy_caterer(n)).collect();
+    println!("{:?} == {}", n_sequence, n_sequence.iter().sum::<BigUint>());
 }
--- src/sequence/figurate/lazy_caterer.rs
+++ src/sequence/figurate/lazy_caterer.rs
@@ -1,18 +1,64 @@
+use crate::big_u;
 use num_bigint::BigUint;
 use std::ops::{Add, Div};
 
+/// Calculates the nth lazy caterer number.
+///
+/// # Arguments
+///
+/// * `n` - A non-negative integer representing the position in the lazy caterer sequence.
+///
+/// # Returns
+///
+/// The nth lazy caterer number as a `BigUint`.
+///
+/// # Examples
+///
+/// ```
+/// use num_bigint::BigUint;
+/// use numberlab::sequence::figurate::lazy_caterer::nth_lazy_caterer;
+///
+/// let result = nth_lazy_caterer(10);
+/// assert_eq!(result, BigUint::from(56u128));
+/// ```
 pub fn nth_lazy_caterer(n: u128) -> BigUint {
-    BigUint::from(n)
+    big_u!(n)
         .pow(2)
-        .add(BigUint::from(n))
-        .add(BigUint::from(2u128))
-        .div(BigUint::from(2u128))
+        .add(big_u!(n))
+        .add(big_u!(2u128))
+        .div(big_u!(2u128))
 }
 
+/// Generates a sequence of lazy caterer numbers up to the given number `n`.
+///
+/// # Arguments
+///
+/// * `n` - A non-negative integer representing the number of terms in the lazy caterer sequence.
+///
+/// # Returns
+///
+/// A vector containing the lazy caterer sequence up to the given number `n`.
+///
+/// # Examples
+///
+/// ```
+/// use num_bigint::BigUint;
+/// use numberlab::sequence::figurate::lazy_caterer::lazy_caterer_sequence;
+///
+/// let sequence = lazy_caterer_sequence(5);
+/// assert_eq!(
+///     sequence,
+///     vec![
+///         BigUint::from(1_u128),
+///         BigUint::from(2_u128),
+///         BigUint::from(4_u128),
+///         BigUint::from(7_u128),
+///         BigUint::from(11_u128)
+///     ]
+/// );
+/// ```
 pub fn lazy_caterer_sequence(n: u128) -> Vec<BigUint> {
     let mut sequence = vec![];
-    for i in 0..n {
-        sequence.push(nth_lazy_caterer(i));
-    }
+    (0..n).for_each(|i| sequence.push(nth_lazy_caterer(i)));
     sequence
 }
