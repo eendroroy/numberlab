d845b8972608b9bd2c00ffd3e409806bc4c96125
indrajit <eendroroy@gmail.com>
2025-02-18 16:19:12 +0600

    updte test coverage

--- src/structure/graph/graph_node_index.rs
+++ src/structure/graph/graph_node_index.rs
@@ -5,12 +5,50 @@ use std::ops::{Index, IndexMut};
 impl<W: GraphWeightTrait, const NODES: usize> Index<usize> for Graph<W, NODES> {
     type Output = String;
 
+    /// Returns a reference to the node value at the given index.
+    ///
+    /// # Arguments
+    ///
+    /// * `index` - The index of the node.
+    ///
+    /// # Returns
+    ///
+    /// * `&Self::Output` - A reference to the node value, which is a `String`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use numberlab::structure::graph::Graph;
+    ///
+    /// let graph = Graph::<f32, 6>::new();
+    /// assert_eq!(graph[0], "0");
+    /// assert_eq!(graph[1], "1");
+    /// ```
     fn index(&self, index: usize) -> &Self::Output {
         &self.nodes[index]
     }
 }
 
 impl<W: GraphWeightTrait, const NODES: usize> IndexMut<usize> for Graph<W, NODES> {
+    /// Returns a mutable reference to the node value at the given index.
+    ///
+    /// # Arguments
+    ///
+    /// * `index` - The index of the node.
+    ///
+    /// # Returns
+    ///
+    /// * `&mut Self::Output` - A mutable reference to the node value, which is a `String`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use numberlab::structure::graph::Graph;
+    ///
+    /// let mut graph = Graph::<f32, 6>::new();
+    /// graph[0] = "A".to_string();
+    /// assert_eq!(graph[0], "A");
+    /// ```
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.nodes[index]
     }
--- tests/structure_graph_test.rs
+++ tests/structure_graph_test.rs
@@ -67,7 +67,7 @@ fn should_find_path_from_source_to_destination_using_bfs() {
 }
 
 #[test]
-fn should_return_distance_from_node_to_node() {
+fn should_return_weight_from_node_to_node() {
     let graph = &mut Graph::from_edges([
         [None, Some(1.0), None, None, None, None],
         [None, None, Some(1.0), None, None, Some(1.0)],
@@ -88,4 +88,48 @@ fn should_return_distance_from_node_to_node() {
     assert_eq!(graph[(4, 1)], Some(1.0));
     assert_eq!(graph[(4, 4)], Some(1.0));
     assert_eq!(graph[(1, 2)], None);
+}
+
+#[test]
+fn should_return_node_label() {
+    let graph = &mut Graph::from(
+        ["A", "B", "C"],
+        [
+            [None, Some(1.0), Some(1.0)],
+            [Some(1.0), None, Some(1.0)],
+            [Some(1.0), Some(1.0), None],
+        ],
+    );
+
+    graph[0] = "D".to_string();
+
+    assert_eq!(graph[0], "D");
+    assert_eq!(graph[1], "B");
+    assert_eq!(graph[2], "C");
+}
+
+#[test]
+fn should_display_graph() {
+    let graph = &Graph::from(
+        ["A", "B", "C", "D", "E", "F"],
+        [
+            [None, Some(1), None, None, None, Some(2)],
+            [None, None, Some(3), None, None, None],
+            [None, None, None, None, None, Some(4)],
+            [None, None, Some(5), None, None, None],
+            [None, None, None, Some(6), None, None],
+            [None, None, None, None, Some(7), None],
+        ],
+    );
+    let dis = "       A   B   C   D   E   F\
+    \n----------------------------\
+    \n  A |  .   1   .   .   .   2 \
+    \n  B |  .   .   3   .   .   . \
+    \n  C |  .   .   .   .   .   4 \
+    \n  D |  .   .   5   .   .   . \
+    \n  E |  .   .   .   6   .   . \
+    \n  F |  .   .   .   .   7   . \
+    \n";
+
+    assert_eq!(format!("{}", graph), dis);
 }
