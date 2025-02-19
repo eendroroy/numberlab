87227b140ade36745259f87f23c0849c14fe0804
indrajit <eendroroy@gmail.com>
2025-02-19 14:07:32 +0600

    implement dijkstra algorithm

--- examples/algorithm_graph.rs
+++ examples/algorithm_graph.rs
@@ -1,16 +1,16 @@
-use numberlab::algorithm::graph::{bfs, dfs};
+use numberlab::algorithm::graph::{bfs, dfs, dijkstra};
 use numberlab::structure::graph::Graph;
 
 fn main() {
     let graph = &Graph::from(
         ["A", "B", "C", "D", "E", "F"],
         [
-            [None, Some(6.3), None, None, None, Some(9.8)],
-            [None, None, Some(1.2), None, None, None],
-            [None, None, None, None, None, Some(2.7)],
+            [None, Some(6.5), None, None, None, Some(9.5)],
+            [None, None, Some(1.5), None, None, None],
+            [None, None, None, None, None, Some(2.5)],
             [None, None, Some(4.5), None, None, None],
-            [None, None, None, Some(2.1), None, None],
-            [None, None, None, None, Some(8.6), None],
+            [None, None, None, Some(2.5), None, None],
+            [None, None, None, None, Some(8.5), None],
         ],
     );
 
@@ -18,10 +18,19 @@ fn main() {
 
     println!();
 
-    println!("DFS - {:?}, BFS - {:?}", dfs(graph, 0, 0), bfs(graph, 0, 0));
-    println!("DFS - {:?}, BFS - {:?}", dfs(graph, 0, 1), bfs(graph, 0, 1));
-    println!("DFS - {:?}, BFS - {:?}", dfs(graph, 0, 2), bfs(graph, 0, 2));
-    println!("DFS - {:?}, BFS - {:?}", dfs(graph, 0, 3), bfs(graph, 0, 3));
-    println!("DFS - {:?}, BFS - {:?}", dfs(graph, 0, 4), bfs(graph, 0, 4));
-    println!("DFS - {:?}, BFS - {:?}", dfs(graph, 0, 5), bfs(graph, 0, 5));
+    println!("DFS {:?}\nBFS {:?}\n", dfs(graph, 0, 0), bfs(graph, 0, 0));
+    println!("DFS {:?}\nBFS {:?}\n", dfs(graph, 0, 1), bfs(graph, 0, 1));
+    println!("DFS {:?}\nBFS {:?}\n", dfs(graph, 0, 2), bfs(graph, 0, 2));
+    println!("DFS {:?}\nBFS {:?}\n", dfs(graph, 0, 3), bfs(graph, 0, 3));
+    println!("DFS {:?}\nBFS {:?}\n", dfs(graph, 0, 4), bfs(graph, 0, 4));
+    println!("DFS {:?}\nBFS {:?}\n", dfs(graph, 0, 5), bfs(graph, 0, 5));
+
+    println!(
+        "{:?}",
+        dijkstra(graph, 0, 3)
+            .iter()
+            .map(|p| format!("{}({})", p.1, p.2))
+            .collect::<Vec<String>>()
+            .join(" --> ")
+    );
 }
--- src/algorithm/graph.rs
+++ src/algorithm/graph.rs
@@ -1,6 +1,6 @@
 use crate::structure::graph::graph_trait::GraphWeightTrait;
 use crate::structure::graph::Graph;
-use std::collections::{BTreeMap, VecDeque};
+use std::collections::{BTreeMap, HashMap, VecDeque};
 
 fn dfs_visit<W: GraphWeightTrait, const NODES: usize>(
     graph: &Graph<W, NODES>,
@@ -129,4 +129,76 @@ pub fn bfs<W: GraphWeightTrait, const NODES: usize>(
     }
 
     vec![]
+}
+
+pub fn dijkstra<W: GraphWeightTrait, const NODES: usize>(
+    graph: &Graph<W, NODES>,
+    source: usize,
+    destination: usize,
+) -> Vec<(usize, String, W)> {
+    let mut parent: HashMap<usize, usize> = HashMap::with_capacity(NODES);
+
+    let mut costs: [Option<W>; NODES] = [None; NODES];
+    let mut explored: [bool; NODES] = [false; NODES];
+
+    costs[source] = Some(W::zero());
+
+    let mut current = source;
+
+    while current < NODES {
+        if destination == current {
+            let mut path = vec![(
+                destination,
+                graph[destination].clone(),
+                costs[destination].unwrap(),
+            )];
+            while parent.contains_key(&current) {
+                let parent_node = parent[&current];
+                path.push((
+                    parent_node,
+                    graph[parent_node].clone(),
+                    costs[parent_node].unwrap(),
+                ));
+
+                current = parent_node;
+            }
+
+            path.reverse();
+
+            return path;
+        }
+        explored[current] = true;
+        let mut to_visit = usize::MAX;
+        let mut to_visit_cost: Option<W> = None;
+        for adj in 0..NODES {
+            match graph[(current, adj)] {
+                Some(weight) => {
+                    match costs[adj] {
+                        None => {
+                            costs[adj] = Some(costs[current].unwrap() + weight);
+                            parent.insert(adj, current);
+                        }
+                        Some(prev_cost) => {
+                            let new_cost = costs[current].unwrap() + weight;
+                            if new_cost <= prev_cost {
+                                costs[adj] = Some(new_cost);
+                                parent.insert(adj, current);
+                            }
+                        }
+                    }
+                    if costs[adj].is_some()
+                        && !explored[adj]
+                        && (to_visit_cost.is_none() || to_visit_cost.unwrap() > costs[adj].unwrap())
+                    {
+                        to_visit_cost = costs[adj];
+                        to_visit = adj;
+                    }
+                }
+                None => {}
+            }
+        }
+        current = to_visit;
+    }
+
+    Vec::<(usize, String, W)>::new()
 }
